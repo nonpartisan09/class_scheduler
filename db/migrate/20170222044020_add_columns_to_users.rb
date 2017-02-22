2cf0ab1a6dedb4ce3b4a638b6be1f3fd93ee1528
Ken Tran <loschorts@gmail.com>
2017-02-21 21:00:58 -0800

    add more user columns

---
+++ db/migrate/20170222044020_add_columns_to_users.rb
@@ -0,0 +1,15 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :users, :phone_number, :string
+  	add_column :users, :f_name, :string, null: false
+  	add_column :users, :l_name, :string, null: false
+  	add_column :users, :type, :string, 
+  		null: false, default: "student"
+  	add_column :users, :language, :string, 
+  		null: false, default: "english"
+
+  	add_index :users, :f_name
+  	add_index :users, :l_name
+  	add_index :users, :phone_number, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222043025) do
+ActiveRecord::Schema.define(version: 20170222044020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",        null: false
+    t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "phone_number"
+    t.string   "f_name",                                     null: false
+    t.string   "l_name",                                     null: false
+    t.string   "type",                   default: "student", null: false
+    t.string   "language",               default: "english", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["f_name"], name: "index_users_on_f_name", using: :btree
+    t.index ["l_name"], name: "index_users_on_l_name", using: :btree
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
