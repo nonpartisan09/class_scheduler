57ddc680f5360585e8faaa29005eeaf00a5b1952
Ken Tran <loschorts@gmail.com>
2017-03-15 13:36:41 -0700

    delete scaffolds and add menu styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,36 +2,14 @@ import React from 'react'
 import {Link} from 'react-router'
 import {translate} from '../utils/translate'
 import {connect} from 'react-redux'
-import {setLanguage} from '../actions/language'
 import Logo from './logo'
+import Navbar from './navbar'
 
-const Header = ({toggleLanguage, language, tr}) => (
+const Header = () => (
 	<header id="app-header" className="row">
 		<Logo/>
-		<nav className="header-links row">
-			<Link to="/student/sign_up">{tr("student_sign_up_link")}
-			</Link>
-			<Link to="/volunteer/sign_up">{tr("volunteer_sign_up_link")}
-			</Link>
-			<Link to="/" onClick={toggleLanguage(language)}>{tr("language_toggle")}</Link>
-		</nav>
+		<Navbar/>
 	</header>
 );
 
-const mapState = ({language}) => ({
-	language: language.default
-})
-
-const mapDispatch = dispatch => ({
-	toggleLanguage: language => e => {
-		e.preventDefault();
-		if (language === "eng") {
-			dispatch(setLanguage("spa"))
-		} else {
-			dispatch(setLanguage("eng"))			
-		}
-	}
-})
-
-// export default Header;
-export default connect(mapState, mapDispatch)(translate("App")(Header));
+export default Header;
---
+++ client/components/navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {translate} from '../utils/translate'
+import {Link} from 'react-router'
+import {connect} from 'react-redux'
+import {setLanguage} from '../actions/language'
+
+class Navbar extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			showing: false
+		}
+	}
+	addToggle(){
+		return (e) => {
+			e.preventDefault();
+			this.setState({showing: !this.state.showing})
+		}
+	}
+	render() {
+		const showMenu = this.state.showing ? "show" : "hide"
+
+		const {toggleLanguage, language, tr} = this.props;
+
+		return (	
+			<nav className="header-nav column">
+				<Link id="header-menu-button" to="" onClick={this.addToggle()}>Menu</Link>
+				<menu id="header-menu" className={`row ${showMenu}`}>
+					<Link to="/student/sign_up">{tr("student_sign_up_link")}</Link>
+					<Link to="/volunteer/sign_up">{tr("volunteer_sign_up_link")}</Link>
+					<Link to="/" onClick={toggleLanguage(language)}>{tr("language_toggle")}</Link>
+				</menu>
+			</nav>
+		);		
+	}
+}
+
+const mapState = ({language}) => ({
+	language: language.default
+})
+
+const mapDispatch = dispatch => ({
+	toggleLanguage: language => e => {
+		e.preventDefault();
+		if (language === "eng") {
+			dispatch(setLanguage("spa"))
+		} else {
+			dispatch(setLanguage("eng"))			
+		}
+	}
+})
+
+export default connect(mapState, mapDispatch)(translate("App")(Navbar));
