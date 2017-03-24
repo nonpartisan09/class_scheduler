a457c35106d534833d54ecf9392da9fbe7f8d8c4
Ken Tran <loschorts@gmail.com>
2017-03-24 16:49:29 -0700

    add logged-in menu state

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -20,22 +20,34 @@ class Navbar extends React.Component {
 	render() {
 		const showMenu = this.state.showing ? "show" : "hide"
 
-		const {toggleLanguage, language, tr} = this.props;
-
-		return (	
-			<nav className="header-nav column">
-				<Link id="header-menu-button" to="" onClick={this.addToggle()}>Menu</Link>
-				<menu id="header-menu" className={`row ${showMenu}`}>
-					<Link to="/student/sign_up">{tr("student_sign_up_link")}</Link>
-					<Link to="/volunteer/sign_up">{tr("volunteer_sign_up_link")}</Link>
-					<Link to="/sign_in">{tr("sign_in_link")}</Link>
-					<Link to="/" onClick={toggleLanguage(language)}>{tr("language_toggle")}</Link>
-				</menu>
-			</nav>
-		);		
+		const {toggleLanguage, language, tr, user} = this.props;
+		if (!user) {
+			return (	
+				<nav className="header-nav column logged-out">
+					<Link id="header-menu-button" to="" onClick={this.addToggle()}>Menu</Link>
+					<menu id="header-menu" className={`row ${showMenu}`}>
+						<Link to="/student/sign_up">{tr("student_sign_up_link")}</Link>
+						<Link to="/volunteer/sign_up">{tr("volunteer_sign_up_link")}</Link>
+						<Link to="/sign_in">{tr("sign_in_link")}</Link>
+						<Link to="/" onClick={toggleLanguage(language)}>{tr("language_toggle")}</Link>
+					</menu>
+				</nav>
+			);		
+		} else {
+			return (
+				<nav className="header-nav column">
+					<Link id="header-menu-button" className="logged-in" to="" onClick={this.addToggle()}>Hi, {user.f_name}!</Link>
+					<menu id="header-menu" className={`logged-in row ${showMenu}`}>
+						<Link to="/logout">{tr("logout")}</Link>
+						<Link to="/" onClick={toggleLanguage(language)}>{tr("language_toggle")}</Link>
+					</menu>
+				</nav>
+			)		
+		}
 	}
 }
 
+const mapState = ({session: {user}}) => ({user})
 const mapDispatch = dispatch => ({
 	toggleLanguage: language => e => {
 		e.preventDefault();
@@ -47,4 +59,4 @@ const mapDispatch = dispatch => ({
 	}
 })
 
-export default connect(null, mapDispatch)(translate("App")(Navbar));
+export default connect(mapState, mapDispatch)(translate("App")(Navbar));
--- client/translations/app.js
+++ client/translations/app.js
@@ -20,5 +20,9 @@ module.exports = {
 	},
 	home: {
 		eng: "home"
+	},
+	logout: {
+		eng: "Logout",
+		spa: "Logoout"
 	}
 }
