fde74d17e9cb03684cdc196ba4156c194b445cd5
Ken Tran <loschorts@gmail.com>
2017-04-08 00:52:17 -0700

    create models

--- miggggg/20170408071134_add_user_info.rb
+++
@@ -1,8 +0,0 @@
-class AddUserInfo < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :users, :first_name, :string, null: false
-  	add_column :users, :last_name, :string, null: false
-  	add_column :users, :language, :string, null: false, default: "en"
-  	add_column :users, :about, :string, null: false
-	end
-end
