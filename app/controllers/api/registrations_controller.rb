84eca7f385dd18366b2ca6864f6d49532e1c0f37
Ken Tran <loschorts@gmail.com>
2017-04-17 21:41:06 -0700

    fix signup

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -3,9 +3,8 @@ class Api::RegistrationsController < Devise::RegistrationsController
 	respond_to :json
 
 	def create
-		super do |resource|
-			sign_in(@user) if @user.valid?
-			redirect_to api_current_user_path and return
+		super do
+			render "api/users/show" and return
 		end
 	end
 
