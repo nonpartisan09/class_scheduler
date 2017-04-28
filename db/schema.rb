01e4c4e718c2e49f1aaca9a77a1dfb688e491ab9
Ken Tran <loschorts@gmail.com>
2017-04-27 21:10:15 -0700

    rename migrations

--- db/schema.rb
+++ db/schema.rb
@@ -62,24 +62,13 @@ ActiveRecord::Schema.define(version: 20170408075114) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.string   "first_name",                            null: false
-    t.string   "last_name",                             null: false
-    t.string   "language",               default: "en", null: false
-    t.string   "about",                                 null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "email",                          null: false
+    t.string "session_token",                  null: false
+    t.string "password_digest",                null: false
+    t.string "first_name",                     null: false
+    t.string "last_name",                      null: false
+    t.string "language",        default: "en", null: false
+    t.string "about",                          null: false
   end
 
 end
