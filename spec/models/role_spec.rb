32b23b13ffdb40ca9876d71281b98af358e0e465
Natacha Ségala <natseg@gmail.com>
2017-09-28 23:34:34 +0100

    A01: Add shoulda matchers gem, add model tests

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_and_belong_to_many(:users) }
+
+  it { is_expected.to have_db_column(:name).of_type(:string) }
+  it { is_expected.to have_db_column(:displayable).of_type(:boolean) }
+  it { is_expected.to have_db_column(:description).of_type(:text) }
+
+  it { is_expected.to validate_presence_of(:url_slug) }
+  it { is_expected.to validate_uniqueness_of(:url_slug) }
+
+  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+  it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
 end
