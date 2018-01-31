422c55b44e8e0c7cc50b6b867c3e962a79da6515
Natacha Ségala <natseg@gmail.com>
2018-01-31 14:55:52 +0000

    Require jquery for active_admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -290,4 +290,7 @@ ActiveAdmin.setup do |config|
   # You can inherit it with own class and inject it for all resources
   #
   # config.order_clause = MyOrderClause
+  #
+  config.register_javascript 'jquery.js'
+  config.register_javascript 'jquery_ujs.js'
 end
