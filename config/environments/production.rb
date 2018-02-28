aeaa03cf1688d5ec0cd0efe77a7888f4c5491886
Natacha Ségala <natseg@gmail.com>
2018-02-28 13:04:42 +0000

    Do not raise errors for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
 
@@ -19,7 +19,7 @@ Rails.application.configure do
       host: 'tutoria.io'
   }
 
-  # SMTP settings for gmail
+  # SMTP settings
   config.action_mailer.smtp_settings = {
       :address              => ENV["SMTP"],
       :domain               => "tutoria.io",
