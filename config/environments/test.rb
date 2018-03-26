c5f2c08f89ada9784bacc96bbd25914ed4875072
Natacha Segala <natseg@gmail.com>
2018-03-26 22:28:07 +0100

    Fix passenger

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
       default_url: nil
   }
 
+  config.base_domain = ENV["DOMAIN_NAME"]
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
