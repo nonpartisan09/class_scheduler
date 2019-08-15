e17c220ba5f4553c71906e2f6a1fdb194f44da24
Felix Melligan <felixmelligan@hotmail.co.uk>
2019-08-14 21:08:25 -0400

    Working on styling testimonial cards

--- app/javascript/components/TestimonialsCarousel.js
+++ app/javascript/components/TestimonialsCarousel.js
@@ -4,15 +4,8 @@ import {
     CardActionArea,
     CardMedia,
     CardContent,
-    CardActions,
-    MobileStepper,
-    Button,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
-import {
-  FaArrowRight,
-  FaArrowLeft
-} from 'react-icons/fa';
 
 class TestimonialsCarousel extends Component {
     constructor(props, context) {
@@ -35,53 +28,50 @@ class TestimonialsCarousel extends Component {
       });
     }
 
+    renderSingleCard(data, position, clickAction) {
+      return(
+        <Card className={ 'cardCarouselCard'+position }>
+          <CardActionArea className='cardCarouselAction' onClick={ clickAction }>
+            <span className='cardCarouselMediaContainer'>
+              <img
+                className='cardCarouselMedia'
+                alt={ data.first_name+' image' }
+                src={ data.img_src }
+                title={ data.first_name+' Testimonial' }
+              />
+              <span className='cardCarouselMediaQuote'>
+                { data.quote }
+              </span>
+            </span>
+            <CardContent className='cardCarouselContent'>
+              <h3 className='cardCarouselFirstName'>
+                { data.first_name }
+              </h3>
+              <p>
+                { data.role }
+              </p>
+              <p>
+                { data.location }
+              </p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      );
+    }
+
+    renderDeck() {
+      console.log(this.props.data.length);
+      return(
+        <span className='cardCarouselContainer'>
+          { this.state.activeStep > 0 ? this.renderSingleCard(this.props.data[this.state.activeStep-1], 'left', this.handleBack) : '' }
+          { this.renderSingleCard(this.props.data[this.state.activeStep], 'center') }
+          { this.state.activeStep < this.props.data.length-1 ? this.renderSingleCard(this.props.data[this.state.activeStep+1], 'right', this.handleNext) : '' }
+        </span>
+      );
+    }
+
     render() {
-        const maxSteps = this.props.data.length;
-        return(
-          <span className='cardCarouselContainer'>
-            <Card className='cardCarouselCard'>
-              <CardActionArea>
-                <CardMedia
-                  className='cardCarouselMedia'
-                  component="img"
-                  alt={ this.props.data[this.state.activeStep].first_name+' image' }
-                  height="140"
-                  image={ this.props.data[this.state.activeStep].img_src }
-                  title={ this.props.data[this.state.activeStep].first_name+' Testimonial' }
-                />
-                <CardContent className='cardCarouselContent'>
-                  <h3 className='cardCarouselFirstName'>
-                    { this.props.data[this.state.activeStep].first_name }
-                  </h3>
-                  <p>
-                    { this.props.data[this.state.activeStep].role }
-                  </p>
-                  <p>
-                    { this.props.data[this.state.activeStep].location }
-                  </p>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <MobileStepper
-                  position='static'
-                  steps={ maxSteps }
-                  nextButton={ (
-                    <Button size="small" onClick={ this.handleNext } disabled={ this.state.activeStep === maxSteps - 1 }>
-                      Next
-                      <FaArrowRight />
-                    </Button>
-                  ) }
-                  backButton={ (
-                    <Button size="small" onClick={ this.handleBack } disabled={ this.state.activeStep === 0 }>
-                      Back
-                      <FaArrowLeft />
-                    </Button>
-                  ) }
-                />
-              </CardActions>
-            </Card>
-          </span>
-        );
+        return( this.renderDeck() );
     }
 }
 
