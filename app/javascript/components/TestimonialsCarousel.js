3cb684d6ba52bb478f4eb92a00544c6a086c8a94
Felix Melligan <felixmelligan@hotmail.co.uk>
2019-08-16 08:05:42 -0400

    completed testimonials component

--- app/javascript/components/TestimonialsCarousel.js
+++ app/javascript/components/TestimonialsCarousel.js
@@ -6,6 +6,10 @@ import {
     CardContent,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import {
+  Player,
+  BigPlayButton
+} from 'video-react';
 
 class TestimonialsCarousel extends Component {
     constructor(props, context) {
@@ -37,32 +41,67 @@ class TestimonialsCarousel extends Component {
         <Card
           className={ 'cardCarouselCard'+position }
           >
-          <CardActionArea className='cardCarouselAction' onClick={ clickAction }>
-            <span className='cardCarouselMediaContainer'>
-              <CardMedia
-                className='cardCarouselMedia'
-                type='img'
-                alt={ data.first_name+' image' }
-                image={ data.img_src }
-                title={ data.first_name+' Testimonial' }
-              />
-              <span className='cardCarouselMediaQuote'>
-                
-                { '"'+data.quote+'"' }
+          { 
+            data.video && position === 'center'
+            ?
+            (
+              <span className='cardCarouselVideoContainer'>
+                <span className='cardCarouselMediaContainer'>
+                  <Player
+                    className='cardCarouselMediaPlayer'
+                    src={ data.video_src }
+                    poster={ data.img_src }
+                    playsInline
+                    fluid={ false }
+                    height='inherit'
+                  >
+                    <BigPlayButton position='center' />
+                  </Player>
+                </span>
+                <CardContent className='cardCarouselContent'>
+                  <h3 className='cardCarouselFirstName'>
+                    { data.first_name }
+                  </h3>
+                  <p>
+                    { data.role }
+                  </p>
+                  <p>
+                    { data.location }
+                  </p>
+                </CardContent>
               </span>
-            </span>
-            <CardContent className='cardCarouselContent'>
-              <h3 className='cardCarouselFirstName'>
-                { data.first_name }
-              </h3>
-              <p>
-                { data.role }
-              </p>
-              <p>
-                { data.location }
-              </p>
-            </CardContent>
-          </CardActionArea>
+            )
+            :
+            (
+              <span>
+                <CardActionArea className='cardCarouselAction' onClick={ clickAction }>
+                  <span className='cardCarouselMediaContainer'>
+                    <CardMedia
+                      className='cardCarouselMedia'
+                      component='img'
+                      alt={ data.first_name+' image' }
+                      src={ data.img_src }
+                      title={ data.first_name+' Testimonial' }
+                    />
+                    <span className='cardCarouselMediaQuote'>
+                      { '"'+data.quote+'"' }
+                    </span>
+                  </span>
+                  <CardContent className='cardCarouselContent'>
+                    <h3 className='cardCarouselFirstName'>
+                      { data.first_name }
+                    </h3>
+                    <p>
+                      { data.role }
+                    </p>
+                    <p>
+                      { data.location }
+                    </p>
+                  </CardContent>
+                </CardActionArea>
+              </span>
+            )
+          }
         </Card>
       );
     }
@@ -71,7 +110,7 @@ class TestimonialsCarousel extends Component {
       return(
         <span className='cardCarouselContainer'>
           { this.state.activeStep > 0 ? this.renderSingleCard(this.props.data[this.state.activeStep-1], 'left', this.handleBack) : '' }
-          { this.renderSingleCard(this.props.data[this.state.activeStep], 'center') }
+          { this.renderSingleCard(this.props.data[this.state.activeStep], 'center', this.handlePlayer) }
           { this.state.activeStep < this.props.data.length-1 ? this.renderSingleCard(this.props.data[this.state.activeStep+1], 'right', this.handleNext) : '' }
         </span>
       );
