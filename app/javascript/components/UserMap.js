a763418c86154c2126b4c9cb181c4d1cafddefe7
Felix Melligan <felixmelligan@hotmail.co.uk>
2019-09-05 15:09:00 -0400

    Completed map component with dummy data

--- app/javascript/components/UserMap.js
+++ app/javascript/components/UserMap.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import { Map, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 
-const addressPoints = [
-    //Data in LatLong to pass to component format [lat, long, intensity]
-];
+import dummyLocationData from '../../../DummyData'; // Needs actual data
  
 class UserMap extends Component {
+    createUserLocationDataCircles(data) {
+      const pointsArray = [];
+      const circleMultiplier = 0.2;
+
+      data.features.forEach((city) => {
+            pointsArray.push(
+                (
+                  <CircleMarker
+                    key={ city.properties.userCity }
+                    className={ 'circle'+city.properties.userCity }
+                    center={ [city.geometry.coordinates[0], city.geometry.coordinates[1]] }
+                    color='#0080FF'
+                    fillColor='#F1592A'
+                    fi
+                    radius={ city.properties.userCityCount*circleMultiplier }
+                  >
+                    <Popup>
+                      <p>{ city.properties.userCity }</p>
+                      <p>{ 'Clients: '+city.properties.clientCount }</p>
+                      <p>{ 'Volunteers: '+city.properties.volunteerCount }</p>
+                    </Popup>
+                  </CircleMarker>
+              )
+            );
+        });
+        return pointsArray;
+    }
+
   render() {
     return (
       <div className='userMapContainer'>
@@ -14,14 +39,10 @@ class UserMap extends Component {
           center={ [40.4637, -3.7492] }
           zoom={ 1.5 }
           style={ {height: '50vh', width: '70vw'} }
+          minZoom={ 1.5 }
+          maxBounds={ [[85, -180], [-85, 180]] }
         >
-          <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={ addressPoints }
-            longitudeExtractor={ m => m[1] }
-            latitudeExtractor={ m => m[0] }
-            intensityExtractor={ m => parseFloat(m[2]) } />
+          { this.createUserLocationDataCircles(dummyLocationData) }
           <TileLayer
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
