b58f65983162c1d202cdd141e9887d3f27d84374
koy <kosatun@aol.com>
2019-09-24 16:50:28 -0700

    fixed small bug in ConversationIndexPage.js after rebasing master

--- app/javascript/components/ConversationPage.js
+++ app/javascript/components/ConversationPage.js
@@ -7,8 +7,6 @@ import Message from './Message';
 import { postData } from './utils/sendData';
 import FormData from './utils/FormData';
 import METHODS from './utils/RestConstants';
-import Header from './reusable/Header';
-import Footer from './reusable/Footer';
 import MessageButtons, { MessageTypes } from './reusable/MessageButtons';
 
 class ConversationPage extends Component {
@@ -16,8 +14,15 @@ class ConversationPage extends Component {
     super(props, context);
 
     this.state = {
+      message: '',
       conversation: props.conversation
     };
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      this.handleMarkAsRead();
+    }, 6000);
   }
 
   render() {
@@ -47,16 +52,39 @@ class ConversationPage extends Component {
       return (
         <Message
           key={ `${index} + ${sent_on}` }
-          sender_first_name={ [sender_first_name] }
+          sender_first_name={ sender_first_name }
           body={ body }
           subject={ subject }
           sentOn={ sent_on }
           avatar={ sender_avatar }
           unread={ unread }
-          conversation={ this.state.conversation }
         />
       );
     });
+  }
+
+  handleMarkAsRead() {
+    const { conversation: { id } } = this.state;
+    const attributes = FormData.from({ id });
+    const requestParams = {
+      url: '/conversation',
+      method: METHODS.PUT,
+      attributes,
+
+      successCallBack: ({ conversation }) => {
+        this.setState({
+          conversation
+        });
+      },
+
+      errorCallBack: (message) => {
+        this.setState({
+          message: message
+        });
+      }
+    };
+
+    return postData(requestParams);
   }
 }
 
