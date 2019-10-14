4af705dcf0ca51a91f3bc03ab7f6abafd4e075a8
mtsheikh <mtsheikh@ualberta.ca>
2019-10-14 12:17:20 -0600

    Pointed all users with null city, latitude and longitude to New Tork

--- app/controllers/api/v1/city_controller.rb
+++ app/controllers/api/v1/city_controller.rb
@@ -3,7 +3,10 @@ module Api
     class CityController < ApplicationController
       def index
         cities = User.select(:city, :latitude, :longitude).group(:city, :latitude, :longitude)
-                     .where.not(:city => nil, :latitude => nil, :longitude => nil)
+
+        cities.where('city IS NULL OR latitude IS NULL OR longitude IS NULL')
+            .update_all(city: "New York", latitude: 40.7127281, longitude: -74.0060152)
+
         render json: cities , serializer: CitiesSerializer, adapter: :attributes, root: ''
       end
     end
