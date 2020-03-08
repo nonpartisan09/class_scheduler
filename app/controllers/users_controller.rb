2f77b7e71dae0aa8f20acae53d377066d5e62f71
barbara <barbara.prusiewicz@gmail.com>
2020-03-08 12:03:09 -1000

    Revert "Map design and improvements"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,73 +1,73 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: :cities
-  before_action :permitted_params, except: :cities
+  before_action :authenticate_user!, :permitted_params
 
   def show
-    redirect_to root_path && return unless current_user
+    unless current_user
+      redirect_to root_path && return
+    end
     @user = User.find_by_url_slug(permitted_params[:url_slug])
 
-    redirect_to root_path && return unless @user.present?
+    unless @user.present?
+      redirect_to root_path && return
+    end
 
-    review = Review.where(author_id: current_user.id, user_id: @user.id).first
+    review = Review.where(:author_id => current_user.id, :user_id => @user.id).first
 
-    review = ReviewDecorator.new(review,
-                                 reviewee_first_name: @user.first_name,
-                                 reviewer: current_user).simple_decorate
+    review = ReviewDecorator.new(review, {
+        reviewee_first_name: @user.first_name,
+        reviewer: current_user
+    }).simple_decorate
 
     @comments = get_ten_last_comments
 
-    @availabilities = Availability.where(user: @user.id)
+    @availabilities = Availability.where(:user => @user.id)
 
     normalize_for_current_user
 
     @data = {
-      currentUser: UserDecorator.new(current_user).simple_decorate,
-      user: UserDecorator.new(@user).decorate,
-      comments: @comments,
-      review: review,
-      availabilities: @availabilities
+        :currentUser => UserDecorator.new(current_user).simple_decorate,
+        :user => UserDecorator.new(@user).decorate,
+        :comments => @comments,
+        :review => review,
+        :availabilities => @availabilities
     }
 
     render :show
   end
 
-  def cities
-    cities = User.cities
-    render json: { cities: cities }, status: :ok
-  end
-
   private
 
   def normalize_for_current_user
-    # TODO: please improve
+    #TODO please improve
     availabilities = []
     @availabilities.each do |availability|
       Time.zone = current_user.timezone
-      first_day = availability.start_time.strftime('%A')
-      second_day = availability.end_time.strftime('%A')
+      first_day = availability.start_time.strftime("%A")
+      second_day = availability.end_time.strftime("%A")
 
       if first_day != second_day
-        split_availability = AvailabilityDecorator.new(availability,
-                                                       timezone: current_user.timezone,
-                                                       user_timezone: @user.timezone,
-                                                       day: first_day,
-                                                       end_time: '23:59').decorate
+        split_availability =  AvailabilityDecorator.new(availability, {
+            :timezone => current_user.timezone,
+            :user_timezone => @user.timezone,
+            :day => first_day,
+            :end_time => "23:59"
+        }).decorate
 
         availabilities << split_availability
 
-        split_availability_2 = AvailabilityDecorator.new(availability,
-                                                         timezone: current_user.timezone,
-                                                         user_timezone: @user.timezone,
-                                                         day: second_day,
-                                                         start_time: '00:00').decorate
+        split_availability_2 = AvailabilityDecorator.new(availability, {
+            :timezone => current_user.timezone,
+            :user_timezone => @user.timezone,
+            :day => second_day,
+            :start_time => "00:00"
+        }).decorate
 
         availabilities << split_availability_2
       else
-        availability = AvailabilityDecorator.new(availability,
-                                                 timezone: current_user.timezone,
-                                                 user_timezone: @user.timezone).decorate
+        availability = AvailabilityDecorator.new(availability, {
+            :timezone => current_user.timezone,
+            :user_timezone => @user.timezone,
+        }).decorate
 
         availabilities << availability
       end
@@ -80,12 +80,13 @@ class UsersController < ApplicationController
     count = received_reviews.count
     ten_last_comments = Review.received_review_search(permitted_params).collect do |review|
       ReviewDecorator.new(review,
-                          reviewee_first_name: @user.first_name).decorate
+          { reviewee_first_name: @user.first_name }
+      ).decorate
     end
 
     @comments = {
-      count: count,
-      ten_last_comments: ten_last_comments
+        count: count,
+        ten_last_comments: ten_last_comments
     }
   end
 
@@ -93,3 +94,4 @@ class UsersController < ApplicationController
     params.permit(:url_slug)
   end
 end
+
