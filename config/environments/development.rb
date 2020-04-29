7ecf915d1ff724cd85be85d57defbd453f9690ff
Koy <kosatun@aol.com>
2020-04-28 18:23:25 -0700

    added locale to mailers to fix reset password bug.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_options = { from: 'noreply@tutoria.io' }
 end
