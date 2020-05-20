164614d82a9c7fd352488d80f9b38d0103d55420
leovcunha <lvcunha@gmail.com>
2020-05-20 14:46:37 +0000

    removed old Google Analytics and Ads Tracking Code which should now be done in Google Tag Manager

--- app/javascript/components/SignUp.js
+++ app/javascript/components/SignUp.js
@@ -10,7 +10,6 @@ import { postData } from './utils/sendData';
 import formatLink from './utils/Link';
 import SignUpSession from './utils/SignUpSession';
 import PageHeader from './reusable/PageHeader';
-import { gtag_formsent_conversion, opts } from './reusable/tracking';
 
 const { SIGN_UP } = UserFormConstants;
 
@@ -45,7 +44,6 @@ function handleUserSignUp() {
         link = '/volunteer_sign_up_completed';
         window.location.href = formatLink(link, userLocale);
       } else if (role === 'client' ) {
-        gtag_formsent_conversion(locale === 'en' ? opts.signup_en : opts.signup_es);
         link = '/search';
         history.push(formatLink(link, userLocale), { signUp: true, currentUser, days });
       }
