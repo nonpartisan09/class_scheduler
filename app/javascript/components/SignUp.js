0b535d5129b3b370f4f1407e6bf073c4bae3ab31
Ashley Elena Giamona <ashleyegiamona@gmail.com>
2020-05-27 12:04:56 -0700

    Fix for Client Sign Up Page 2 missing distance filter and client data (#105)
    * removed old Google Analytics and Ads Tracking Code which should now be done in Google Tag Manager
    
    * Signup process now uses href to load next page and url query string to store signup state
    
    * Removed references to location.state for signup process in SearchBar and NewAvailability components
    
    Co-authored-by: leovcunha <lvcunha@gmail.com>

--- app/javascript/components/SignUp.js
+++ app/javascript/components/SignUp.js
@@ -10,7 +10,6 @@ import { postData } from './utils/sendData';
 import formatLink from './utils/Link';
 import SignUpSession from './utils/SignUpSession';
 import PageHeader from './reusable/PageHeader';
-import { gtag_formsent_conversion, opts } from './reusable/tracking';
 
 const { SIGN_UP } = UserFormConstants;
 
@@ -19,7 +18,7 @@ const ignoredFields = [
 ];
 
 function handleUserSignUp() {
-  const { match: { params: { role } }, history } = this.props;
+  const { match: { params: { role } } } = this.props;
   const { currentUser, currentUser: { locale } } = this.props;
 
   const updatedUser =  _.reduce(currentUser, (memo, value, key) => {
@@ -37,7 +36,7 @@ function handleUserSignUp() {
     url: restfulUrl,
     attributes,
     method: 'POST',
-    successCallBack: ({ currentUser, days }) => {
+    successCallBack: () => {
       const userLocale = updatedUser.locale || '';
       let link = '';
 
@@ -45,9 +44,9 @@ function handleUserSignUp() {
         link = '/volunteer_sign_up_completed';
         window.location.href = formatLink(link, userLocale);
       } else if (role === 'client' ) {
-        gtag_formsent_conversion(locale === 'en' ? opts.signup_en : opts.signup_es);
         link = '/search';
-        history.push(formatLink(link, userLocale), { signUp: true, currentUser, days });
+        const signupQuery = '?signup=true';
+        window.location.href = formatLink(link + signupQuery, userLocale);
       }
     },
 
