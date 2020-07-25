f6e50bdc2c810984d903ba95950b31785d8a78c7
kscali <kosatun@aol.com>
2020-07-24 18:59:48 -0700

    Youtube video (#134)
    * added last name field to forms (#125)
    
    * added last name field to forms
    
    * Added space between first and last name. Changed user profile from First Name to Name.
    
    * Dropdown for City/State form (#121)
    
    * added select options to state and countries to normalize forms.
    
    * updated yarn.lock
    
    * removed US territories from state. Add correct label for State/Region.
    
    * embedded youtube video on home page

---
+++ app/javascript/components/TutoriaVideo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TutoriaVideo = () => (
+  <div className='video-responsive'>
+    <iframe 
+      title='How to use Tutoria'
+      width="960" 
+      height="639" 
+      src="https://www.youtube.com/embed/SixpNhP7-Ng" 
+      frameBorder="0" 
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+      allowFullScreen 
+    />
+  </div> 
+);
+
+export default TutoriaVideo;
