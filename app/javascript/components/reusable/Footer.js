a2a15002bd6aa8d45d7070d437213dfa8ccd0dc6
kscali <kosatun@aol.com>
2020-08-11 19:17:47 -0700

    Whatsapp logo (#147)
    * Bump elliptic from 6.5.0 to 6.5.3
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.0 to 6.5.3.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.0...v6.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * added whatsapp logo to footer
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Koy Saeteurn <koy_saeteurn@epam.com>

--- app/javascript/components/reusable/Footer.js
+++ app/javascript/components/reusable/Footer.js
@@ -10,7 +10,7 @@ import {
   FaRegCopyright,
   FaLinkedin,
   FaEnvelope,
-  FaPhone
+  FaWhatsapp
 } from 'react-icons/fa';
 
 import formatLink from '../utils/Link';
@@ -135,7 +135,7 @@ class Footer extends Component {
               <a
                 href={ 'tel: +1(929)-359-3985' }
               >
-                <FaPhone
+                <FaWhatsapp
                   size={ size }
                   label={ (
                     <FormattedMessage
