eaec7740686713ed6c351c6743ced3b498c42ed8
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-08-24 00:34:07 -0400

    Map (#168)
    * Uncomment homepage code for map
    
    * Add radius variable to render method in usermap
    
    * Add counts for active users counts i.e client, volunteer and all
    
    * Add helper methods and counts method that returns the object including counts of all active users
    
    * Add custom route for counts
    
    * Add fetch for counts in usermap and add logic to calculate radius of the marker based on active users divided by total users
    
    * Add max zoom restriction to the user map
    
    * Remove debugger form usermap
    
    * Fix map issue for logged out users by adding counts function in excepted list
    
    Co-authored-by: kscali <kosatun@aol.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  include AvailabilitiesSorter
-  
-  before_action :authenticate_user!, except: :cities
-  before_action :permitted_params, except: :cities
+  before_action :authenticate_user!, except: [:cities, :counts]
+  before_action :permitted_params, except: [:cities, :counts]
 
   def show
     redirect_to root_path && return unless current_user
