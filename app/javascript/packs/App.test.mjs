a37367324b6701509e6f6420f7180ed9b26443d5
DanBourdier <danbourdier@aol.com>
2020-09-23 00:16:40 -0400

    added enzyme adapter to app.test.mjs

--- app/javascript/packs/App.test.mjs
+++ app/javascript/packs/App.test.mjs
@@ -1,5 +1,9 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './application';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 // eslint-disable-next-line no-undef
 test('renders without error', () => {
