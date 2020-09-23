44eb5d935e97c3b721b2d3e1f6295c3f70ea4f57
DanBourdier <danbourdier@aol.com>
2020-09-23 01:11:31 -0400

    first successful jest-enzyme test completed

--- app/javascript/packs/App.test.mjs
+++ app/javascript/packs/App.test.mjs
@@ -4,8 +4,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import App from './application';
 
 Enzyme.configure({ adapter: new Adapter() });
-
+ 
 // eslint-disable-next-line no-undef
 test('renders without error', () => {
-  
-});
+  const wrapper = Enzyme.shallow(<App />);
+
+  expect(wrapper).toBeTruthy();
+}); 
