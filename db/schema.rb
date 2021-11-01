2d8546d2ff71835126a170336da06c0714f686b7
andysmitharng <37690831+andysmitharng@users.noreply.github.com>
2021-11-01 18:13:29 -0400

    Privacy policy admin tab (#252)
    created privacy policy tab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_140413) do
+ActiveRecord::Schema.define(version: 2021_08_29_163738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,13 @@ ActiveRecord::Schema.define(version: 2020_11_26_140413) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "privacy_policy", force: :cascade do |t|
+    t.text "description"
+    t.text "spanish_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "programs", force: :cascade do |t|
     t.string "name"
     t.string "url_slug"
@@ -161,6 +168,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_140413) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "spanish_description"
   end
 
   create_table "users", force: :cascade do |t|
